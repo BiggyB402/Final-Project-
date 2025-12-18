93683afa7dac70cd2ee00153c7aa27983de2eb49
BiggyB402 <sheanes@iu.edu>
2025-12-17 22:18:12 -0500

    New Changes

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,18 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Needed if you want to display on UI Text
 
-public class Timer : MonoBehaviour
+public class CounterTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text timerText; // Optional: Assign a UI Text element in the Inspector
+    public bool autoStart = true; // Start counting automatically
+    public bool showInConsole = true; // Log time in Console
+
+    private float elapsedTime = 0f; // Time in seconds
+    private bool isRunning = false;
+
     void Start()
     {
-        
+        if (autoStart)
+            StartTimer();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isRunning)
+        {
+            elapsedTime += Time.deltaTime; // Add time since last frame
+
+            // Convert to minutes:seconds format
+            int minutes = Mathf.FloorToInt(elapsedTime / 60);
+            int seconds = Mathf.FloorToInt(elapsedTime % 60);
+
+            string timeFormatted = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+            // Display in UI if assigned
+            if (timerText != null)
+                timerText.text = timeFormatted;
+
+            // Optionally log to console
+            if (showInConsole)
+                Debug.Log("Timer: " + timeFormatted);
+        }
+    }
+
+    // Public methods to control the timer
+    public void StartTimer()
+    {
+        isRunning = true;
+    }
+
+    public void PauseTimer()
+    {
+        isRunning = false;
+    }
+
+    public void ResetTimer()
+    {
+        elapsedTime = 0f;
+        if (timerText != null)
+            timerText.text = "00:00";
+    }
+
+    public float GetElapsedTime()
+    {
+        return elapsedTime;
     }
 }
